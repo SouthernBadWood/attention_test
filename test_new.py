d2773f7475c0d740c32615bdd1228f0dec67eda8
SouthernBadWood <1452360354@qq.com>
2025-04-02 17:19:54 +0800

    增加第五次修改

---
+++ test_new.py
@@ -0,0 +1,10 @@
+for i in range(2):
+    print(i)
+    if i == 1:
+        print("i is 1")
+    else:
+        print("i is not 1")
+    if i == 2:
+        print("i is 2")
+    else:
+        print("i is not 2")
